cd6aaf47ec8b95220b78766c68bc7b4a80a5309c
yimango <yiwu.wang@gmail.com>
2022-08-20 15:06:48 -0400

    added functionality for adding words to wordbank

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
-document.getElementById('addWord').addEventListener("click", newfunction);
+document.getElementById('addWord').addEventListener("click", showadd);
+document.getElementById('addthis').addEventListener("click", combine);
+var wordbank = [];
 
-function newfunction(){
-    document.getElementById('addWord').innerHTML = "Something happened"
+function showadd(){
+    document.getElementById('newword').hidden = false;
+    document.getElementById('newdef').hidden = false;
+    document.getElementById('addWord').hidden = true;
+    document.getElementById('addthis').hidden = false;
+}
+
+function addword(){
+    var word = document.getElementById('newword').value;
+    var def = document.getElementById('newdef').value;
+    return [word, def]
+}
+
+function createword(arr){
+    var obj = {
+        word: arr[0],
+        def: arr[1]
+    }
+    wordbank.push(obj);
+}
+
+function combine(){
+    createword(addword());
 }
