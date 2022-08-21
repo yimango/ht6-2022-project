5df316a24ead94e08941227529294102fcba5ea0
Rebbi Xu <rebbi.xu123@gmail.com>
2022-08-20 20:15:05 -0400

    sorting data into dictionary

--- vocabTidy.py
+++ vocabTidy.py
@@ -1,6 +1,7 @@
-myfile = open("vocab.txt", "r")
-linelist=[]
+myfile = open("vocabraw.txt", "r")
+#wordlist=[]
 linelist2=[]
+worddict = {}
 
 for line in myfile:
     #testing1
@@ -8,9 +9,19 @@ for line in myfile:
         #t, e, s, t, i, n, g, 1
         linelist2.append(char)
     del linelist2[0:5]
+    del linelist2[-1:]
+    
     linelist2 = ''.join(linelist2)
-    linelist.append(linelist2)
+    linelist2 = linelist2.split('\t')
+    #wordlist.append(linelist2)
+    
+
+    worddict['word'] = linelist2[0]
+    worddict['trans'] = linelist2[1]
+    worddict['use'] = linelist2[2]
+
+    print(worddict)
+
     linelist2=[]
-print(linelist)
 
 myfile.close()
